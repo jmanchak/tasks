e62181ace6b80ab40fe76acd2295237b357fb142
Jessica Manchakq <jessicamanchak@wifi-roaming-128-4-132-74.wifi.udel.edu>
2023-02-20 10:47:57 -0500

    Task 4 - Functions

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,7 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    return (temperature - 32) * (5 / 9);
 }
 
 /**
@@ -12,7 +12,18 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let numSum: number;
+    numSum = 0;
+    if (first >= 0) {
+        numSum += first;
+    }
+    if (second >= 0) {
+        numSum += second;
+    }
+    if (third >= 0) {
+        numSum += third;
+    }
+    return numSum;
 }
 
 /**
@@ -20,7 +31,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -28,7 +39,8 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    let messageLength: number = message.length;
+    return message.charAt(messageLength - 1) === "?" ? true : false;
 }
 
 /**
@@ -37,5 +49,11 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word === "yes" || word === "YES") {
+        return true;
+    } else if (word === "no" || word === "NO") {
+        return false;
+    } else {
+        return null;
+    }
 }
