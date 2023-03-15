dece9e96e1d762444d627c31b2e5dbe599a67718
Jessica Manchak <jmanchak@udel.edu>
2023-03-15 11:43:37 -0400

    Task 9 - Choose Team

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -14,18 +14,18 @@ export function ChooseTeam(): JSX.Element {
     const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
-    function chooseMember() {
-        /*
+    function chooseMember(newMember: string) {
+        const addedMember = [...team, newMember];
+
         if (!team.includes(newMember)) {
-            team.push(newMember);
+            setTeam(addedMember);
+            setAllOptions(allOptions);
         }
-        */
     }
 
     function clearTeam() {
-        /*
-        team = [];
-        */
+        setTeam([]);
+        setAllOptions(allOptions);
     }
 
     return (
@@ -36,7 +36,10 @@ export function ChooseTeam(): JSX.Element {
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button
+                                onClick={() => chooseMember(option)}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>
