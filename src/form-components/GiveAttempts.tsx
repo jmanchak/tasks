138556575761c81804aa2f9599c826adaed961ba
Jessica Manchak <jmanchak@udel.edu>
2023-03-22 11:13:14 -0400

    Task 10 - Give Attempts

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,41 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [numAttempts, setNumAttempts] = useState<number>(3);
+    const [requests, setRequests] = useState<string>(" ");
+
+    function updateReq(event: React.ChangeEvent<HTMLInputElement>) {
+        setRequests(event.target.value);
+    }
+    function updateGain(req: string) {
+        if (!isNaN(parseInt(req))) {
+            const add = parseInt(req);
+            setNumAttempts(numAttempts + add);
+        } else {
+            setNumAttempts(numAttempts);
+        }
+    }
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <div>Attempts: {numAttempts}</div>
+            <Form.Group controlId="formGiveAttempts">
+                <Form.Label>Requested Attempts: </Form.Label>
+                <Form.Control
+                    type="number"
+                    value={requests}
+                    onChange={updateReq}
+                />
+            </Form.Group>
+            <Button
+                onClick={() => setNumAttempts(numAttempts - 1)}
+                disabled={numAttempts < 1}
+            >
+                Use
+            </Button>
+            <Button onClick={() => updateGain(requests)}>Gain</Button>
         </div>
     );
 }
