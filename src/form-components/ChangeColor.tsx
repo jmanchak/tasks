b7d4c785dadc5957e58e20c5f9c5b850c5b9751f
Jessica Manchak <jmanchak@udel.edu>
2023-03-22 11:12:15 -0400

    Task 10 - Change Color

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,53 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): JSX.Element {
+    const [color, setColor] = useState<string>("blue");
+    const colorsList = [
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "purple",
+        "black",
+        "brown"
+    ];
+
+    function updateColor(event: React.ChangeEvent<HTMLInputElement>) {
+        setColor(event.target.value);
+    }
+
     return (
         <div>
             <h3>Change Color</h3>
+            <div>
+                {colorsList.map((color: string) => (
+                    <Form.Check
+                        key={color}
+                        inline
+                        type="radio"
+                        onChange={updateColor}
+                        label={color}
+                        value={color}
+                        checked={false}
+                        style={{ backgroundColor: color }}
+                    />
+                ))}
+            </div>
+            <div>
+                You have chosen{" "}
+                <span
+                    data-testid="colored-box"
+                    style={{
+                        backgroundColor: color,
+                        width: "50px"
+                    }}
+                >
+                    {color}
+                </span>
+                .
+            </div>
         </div>
     );
 }
