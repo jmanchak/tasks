b6037888e992f2660a1d62c0e8d90bdf58601676
Jessica Manchak <jmanchak@udel.edu>
2023-03-22 11:12:34 -0400

    Task 10 - Check Answer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [givenAnswer, setGivenAnswer] = useState<string>("");
+
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setGivenAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="givenShortAnswer">
+                <Form.Label>
+                    Your answer is:{" "}
+                    {expectedAnswer === givenAnswer ? (
+                        <span>✔️</span>
+                    ) : (
+                        <span>❌</span>
+                    )}
+                </Form.Label>
+                <Form.Control value={givenAnswer} onChange={updateAnswer} />
+            </Form.Group>
         </div>
     );
 }
